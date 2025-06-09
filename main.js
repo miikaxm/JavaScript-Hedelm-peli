e9f9a0024022873add1906d2ede91534dea8099c
Miika Valkonen <miika.valkonen08@gmail.com>
2025-06-09 18:33:48 +0300

    Add coin image and update game layout with betting options

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 document.getElementById("playButton").addEventListener("click", roll)
+document.getElementById("betButton1").addEventListener("click" , betSize1)
+document.getElementById("betButton2").addEventListener("click" , betSize2)
+document.getElementById("betButton3").addEventListener("click" , betSize3)
 
 let money = 50
 let bet = 1
+
+const boxes = [
+    { number: "1", value: 0},
+    { number: "2", value: 0},
+    { number: "3", value: 0},
+    { number: "4", value: 0}
+]
 
 function update(){
     document.getElementById("money").innerText = "Rahaa: " + money + "€"
@@ -12,37 +22,48 @@ function randomNumber(){
     return Math.floor(Math.random()* 5 + 1)
 }
 
-function roll(){
-    if (money >= bet){
+function roll() {
+    if (money >= bet) {
         money -= bet
-        rolledNumber1 = randomNumber()
-        rolledNumber2 = randomNumber()
-        rolledNumber3 = randomNumber()
-        rolledNumber4 = randomNumber()
-        rolledImage1 = "img/symbol_" + rolledNumber1 + ".png"
-        rolledImage2 = "img/symbol_" + rolledNumber2 + ".png"
-        rolledImage3 = "img/symbol_" + rolledNumber3 + ".png"
-        rolledImage4 = "img/symbol_" + rolledNumber4 + ".png"
-        document.getElementById("1").setAttribute("src", rolledImage1)
-        document.getElementById("2").setAttribute("src", rolledImage2)
-        document.getElementById("3").setAttribute("src", rolledImage3)
-        document.getElementById("4").setAttribute("src", rolledImage4)
-        update()
-        checkWin()
+
+        for (let i = 0; i < 4; i++) {
+            boxes[i]["value"] = randomNumber()
+        }
+
+        for (let i = 0; i < 4; i++) {
+            let img = "img/symbol_" + boxes[i]["value"] + ".png"
+            document.getElementById((i + 1).toString()).setAttribute("src", img)
+        }
         update()
     }
 }
 
+function betSize1() {
+    bet = 1;
+    update();
+}
+
+function betSize2() {
+    bet = 2;
+    update();
+}
+
+function betSize3() {
+    bet = 3;
+    update();
+}
+
 function checkWin(){
-    if (rolledNumber1 == 5 && rolledNumber2 == 5 && rolledNumber3 == 5 && rolledNumber4 == 5){
+    if (boxes[0]["value"] == 5 && boxes[1]["value"] == 5 && boxes[2]["value"] == 5 && boxes[3]["value"] == 5){
         money += bet*10
-    } else if (rolledNumber1 == 4 && rolledNumber2 == 4 && rolledNumber3 == 4 && rolledNumber4 == 4){
+    } else if (boxes[0]["value"] == 4 && boxes[1]["value"] == 4 && boxes[2]["value"] == 4 && boxes[3]["value"] == 4){
         money += bet*6
-    } else if (rolledNumber1 == 3 && rolledNumber2 == 3 && rolledNumber3 == 3 && rolledNumber4 == 3){
+    } else if (boxes[0]["value"] == 3 && boxes[1]["value"] == 3 && boxes[2]["value"] == 3 && boxes[3]["value"] == 3){
         money += bet*5
-    } else if (rolledNumber1 == 2 && rolledNumber2 == 2 && rolledNumber3 == 2 && rolledNumber4 == 2){
+    } else if (boxes[0]["value"] == 2 && boxes[1]["value"] == 2 && boxes[2]["value"] == 2 && boxes[3]["value"] == 2){
         money += bet*4
-    } else if (rolledNumber1 == 1 && rolledNumber2 == 1 && rolledNumber3 == 1 && rolledNumber4 == 1){
+    } else if (boxes[0]["value"] == 1 && boxes[1]["value"] == 1 && boxes[2]["value"] == 1 && boxes[3]["value"] == 1){
         money += bet*3
     }
+
 }
